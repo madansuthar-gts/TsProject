68b9df3408b2328740a20292b2e85f43682c14ff
madansuthar-gts <madan.suthar@gtsinfosoft.com>
2025-05-13 12:16:43 +0530

    Update .gitignore to ignore all node_modules directories

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import path from 'path';
 const main = async () => {
   const filePath = path.resolve('hello.txt');
   console.log("Hello world")
+  
   await fs.writeFile(filePath, 'Hello Sir Madan!\n');
   const content = await fs.readFile(filePath, 'utf-8');
   console.log('File Content:', content);
